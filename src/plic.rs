181224496a9c6674c7686bd85e145ee02e190b38
nanameki1213 <nanamekitaishin1213@icloud.com>
2025-03-20 16:52:18 +0900

    [add] plicのクレームフィールドの読み書きを追加

--- src/plic.rs
+++ src/plic.rs
@@ -7,11 +7,13 @@ pub const PLIC_ADDR: usize = 0xc000000;
 
 const PLIC_PRIORITY_OFFSET: usize = 0x0;
 const PLIC_ENABLE_BITS_OFFSET: usize = 0x2000;
+const PLIC_CLAIM_OFFSET: usize = 0x200004;
 
 const PLIC_WORD_SIZE_BITS: usize = 32;
 const PLIC_WORD_SIZE: usize = 4;
 const PLIC_SOURCE_NUM: usize = 1024;
 const PLIC_ENABLE_BITS_SIZE: usize = (PLIC_SOURCE_NUM / PLIC_WORD_SIZE_BITS) * PLIC_WORD_SIZE;
+const PLIC_CLAIM_SIZE: usize = 0x1000;
 
 pub fn set_plic_enable(hart: usize, source: usize) {
     let row = source % 32;
@@ -39,6 +41,22 @@ pub fn set_plic_thresholds(hart: usize, thresholds: usize) {
     
     unsafe {
         core::ptr::write_volatile(plic_thresholds_address, thresholds as u32);
+    }
+}
+
+pub fn get_plic_claim(hart: usize) -> usize {
+    let plic_claim_address = (PLIC_ADDR + PLIC_CLAIM_OFFSET + PLIC_CLAIM_SIZE * hart) as *mut u32;
+
+    unsafe {
+        core::ptr::read_volatile(plic_claim_address) as usize
+    }
+}
+
+pub fn set_plic_claim(hart: usize, irq: usize) {
+    let plic_claim_address = (PLIC_ADDR + PLIC_CLAIM_OFFSET + PLIC_CLAIM_SIZE * hart) as *mut u32;
+
+    unsafe {
+        core::ptr::write_volatile(plic_claim_address, irq as u32);
     }
 }
 
