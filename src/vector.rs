307386d9e103024792936ec8fae098afb5aafd5c
nanameki1213 <nanamekitaishin1213@icloud.com>
2025-03-21 11:05:22 +0900

    [fix] virtual_addressのまえにアドレス参照がついていたので修正

--- src/vector.rs
+++ src/vector.rs
@@ -360,7 +360,7 @@ pub fn exception_handler(mode: u8, sp: usize) {
 fn write_access(virtual_address: usize, value: u64) {
     if (ns16550::NS16550_ADDR..=ns16550::NS16550_ADDR + 0x100).contains(&(virtual_address)) {
         ns16550::emulate_write_ns16550(virtual_address - ns16550::NS16550_ADDR, value as u32);
-    } else if (virtio::VIRTIO_MMIO_DEFAULT_ADDRESS..=virtio::VIRTIO_MMIO_DEFAULT_ADDRESS + 0x1000).contains(&(&virtual_address)) {
+    } else if (virtio::VIRTIO_MMIO_DEFAULT_ADDRESS..=virtio::VIRTIO_MMIO_DEFAULT_ADDRESS + 0x1000).contains(&(virtual_address)) {
         virtio::emulate_write_virtio(virtual_address - virtio::VIRTIO_MMIO_DEFAULT_ADDRESS, value as u32);
     } else {
         println!("write access data abort");
@@ -375,7 +375,7 @@ fn read_access(virtual_address: usize, dst_register_idx: usize, registers: &mut 
     if (ns16550::NS16550_ADDR..=ns16550::NS16550_ADDR + 0x100).contains(&(virtual_address)) {
         registers[dst_register_idx] =
             ns16550::emulate_read_ns16550(virtual_address - ns16550::NS16550_ADDR).unwrap() as u64;
-    } else if (virtio::VIRTIO_MMIO_DEFAULT_ADDRESS..=virtio::VIRTIO_MMIO_DEFAULT_ADDRESS + 0x1000).contains(&(&virtual_address)) {
+    } else if (virtio::VIRTIO_MMIO_DEFAULT_ADDRESS..=virtio::VIRTIO_MMIO_DEFAULT_ADDRESS + 0x1000).contains(&(virtual_address)) {
         registers[dst_register_idx] =
             virtio::emulate_read_virtio(virtual_address - virtio::VIRTIO_MMIO_DEFAULT_ADDRESS).unwrap() as u64;
     } else {
