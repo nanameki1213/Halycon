8b66bcb52558cfd7c7b5aa58758bfc19022fa87f
nanameki1213 <nanamekitaishin1213@icloud.com>
2025-03-31 21:56:29 +0900

    [add] 短縮命令かどうかの判定を追加

--- src/instruction.rs
+++ src/instruction.rs
@@ -15,10 +15,13 @@ impl Instruction {
     const RS2_MASK: u32 = ((1 << 5) - 1) << Self::RS2_OFFSET;
     const FUNCT12_OFFSET: u32 = 20;
     const FUNCT12_MASK: u32 = ((1 << 12) - 1) << Self::FUNCT12_OFFSET;
+    const COMPRESSION_OFFSET: u32 = 0;
+    const COMPRESSION_MASK: u32 = ((1 << 2) - 1) << Self::COMPRESSION_OFFSET;
     
     const OPCODE_CSR: usize = 0b1110011;
     const FUNCT3_CSRRS: usize = 0b010;
     const FUNCT3_CSRRW: usize = 0b001;
+    const COMPRESSED_INSTRUCTION: usize = 0b01;
 
     pub const fn new(instruction: u32) -> Self {
         Self(instruction)
@@ -48,8 +51,17 @@ impl Instruction {
         ((self.0 & Self::FUNCT12_MASK) >> Self::FUNCT12_OFFSET) as usize
     }
 
+    pub fn get_compression(&mut self) -> usize {
+        ((self.0 & Self::COMPRESSION_MASK) >> Self::COMPRESSION_OFFSET) as usize
+    }
+
     pub fn is_csrrs_instruction(&mut self) -> bool {
         self.get_opcode() == Self::OPCODE_CSR &&
         self.get_funct3() == Self::FUNCT3_CSRRS
+    }
+
+    pub fn is_compression_instruction(&mut self) -> bool {
+        let compression = self.get_compression();
+        compression == Self::COMPRESSED_INSTRUCTION
     }
 }
