6945947d652c4231c07d1566de4c4be677652c56
nanameki1213 <nanamekitaishin1213@icloud.com>
2025-05-14 12:28:23 +0900

    [add] 仮想virtioのデバッグ文を追加

--- src/mmio/virtio.rs
+++ src/mmio/virtio.rs
@@ -187,18 +187,18 @@ pub fn is_queue_available(index: u32) -> bool {
 const VIRTIO_MMIO_EMULATE_OFFSET: usize = 0x2000;
 
 pub fn emulate_read_virtio(offset: usize) -> Result<u32, ()> {
-    println!("read: {}", offset);
     let address = (VIRTIO_MMIO_DEFAULT_ADDRESS + VIRTIO_MMIO_EMULATE_OFFSET + offset) as *mut u32;
 
     let value = unsafe {
         core::ptr::read_volatile(address)
     };
 
+    println!("read: {:#X}, {:#X}", offset, value);
     Ok(value)
 }
 
 pub fn emulate_write_virtio(offset: usize, value: u32) {
-    println!("write: {}, {}", offset, value);
+    println!("write: {:#X}, {}", offset, value);
     let address = (VIRTIO_MMIO_DEFAULT_ADDRESS + VIRTIO_MMIO_EMULATE_OFFSET + offset) as *mut u32;
 
     unsafe {
