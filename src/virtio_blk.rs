042ce3f666ecc4661b737f887246f6a390a6067e
nanameki1213 <nanamekitaishin1213@icloud.com>
2025-05-31 19:34:03 +0900

    [add] virtio block deviceのfeatureビットマクロを追加

--- src/virtio_blk.rs
+++ src/virtio_blk.rs
@@ -18,6 +18,18 @@ pub const VIRTIO_BLK_S_OK: usize = 0;
 pub const VIRTIO_BLK_S_IOERR: usize = 1;
 pub const VIRTIO_BLK_S_UNSUPP: usize = 2;
 
+// block device feature bits
+pub const VIRTIO_BLK_F_SIZE_MAX: usize = 1 << 1;
+pub const VIRTIO_BLK_F_SEG_MAX: usize = 1 << 2;
+pub const VIRTIO_BLK_F_GEOMETRY: usize = 1 << 4;
+pub const VIRTIO_BLK_F_RO: usize = 1 << 5;
+pub const VIRTIO_BLK_F_BLK_SIZE: usize = 1 << 6;
+pub const VIRTIO_BLK_F_FLUSH: usize = 1 << 9;
+pub const VIRTIO_BLK_F_TOPOLOGY: usize = 1 << 10;
+pub const VIRTIO_BLK_F_MQ: usize = 1 << 12;
+pub const VIRTIO_BLK_F_DISCARD: usize = 1 << 13;
+pub const VIRTIO_BLK_F_WRITE_ZEROES: usize = 1 << 14;
+
 #[repr(C)]
 pub struct VirtioBlkReq {
     pub req_type: u32,
