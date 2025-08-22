f9ddf24063befb164468ed6b797b5b15c9eff352
nanameki1213 <nanamekitaishin1213@icloud.com>
2025-08-22 19:15:38 +0900

    [change] string_utils.rs

--- src/string_utils.rs
+++ src/string_utils.rs
@@ -1,4 +1,5 @@
 use core::str;
+use crate::println;
 
 // allocが使えない環境下での文字列操作関数
 pub unsafe fn hex_ptr_to_usize(ptr: *const u8) -> Result<usize, ()> {
@@ -9,18 +10,14 @@ pub unsafe fn hex_ptr_to_usize(ptr: *const u8) -> Result<usize, ()> {
     }
 
     let slice = core::slice::from_raw_parts(ptr, len);
-    let s = match str::from_utf8(slice) {
-        Ok(s) => s,
-        Err(_) => return Err(()),
-    };
-
-    let s = s.strip_prefix("0x").or_else(|| s.strip_prefix("0X")).unwrap_or(s);
-
-    usize::from_str_radix(s, 16).map_err(|_| ())
+    match slice_to_usize(slice) {
+        Ok(value) => Ok(value),
+        Err(_) => Err(()),
+    }
 }
 
-pub unsafe fn hex_ptr_to_usize_length(ptr: *const u8, len: usize) -> Result<usize, ()> {
-    let slice = core::slice::from_raw_parts(ptr, len);
+pub unsafe fn slice_to_usize(slice: &[u8]) -> Result<usize, ()> {
+    println!("{:?}", slice);
     let s = match str::from_utf8(slice) {
         Ok(s) => s,
         Err(_) => return Err(()),
