680d894dc2acd9d2a3aaa14368dd1bfb043093d3
nanameki1213 <nanamekitaishin1213@icloud.com>
2025-10-12 13:58:01 +0900

    [add] intrusive linked listを実装

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -11,5 +11,40 @@ impl LinkedList {
         self.head.is_null()
     }
 
-    pub fn push(&mut self, )
+    pub fn push(&mut self, value: *mut usize) {
+        *value = self.head as usize;
+        self.head = value;
+    }
+
+    pub fn pop(&mut self) -> Option<*mut usize> {
+        if self.is_empty() {
+            None
+        } else {
+            let value = self.head;
+            self.head = unsafe { *(self.head) as *mut usize };
+            Some(value)
+        }
+    }
+
+    pub fn iter(&self) -> LinkedListIterator {
+
+    }
+}
+
+pub struct LinkedListIterator {
+    current: *mut usize,
+}
+
+impl Iterator for LinkedListIteretor {
+    type Item = *mut usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current.is_null() {
+            None
+        } else {
+            let value = self.current;
+            self.current = unsafe { *value as *mut usize };
+            Some(value)
+        }
+    }
 }
