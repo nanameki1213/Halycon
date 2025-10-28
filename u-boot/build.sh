449a38b8e6b7fd4ea9e0d3397c0e77b092e7ab7c
nanameki1213 <nanamekitaishin1213@icloud.com>
2025-10-28 17:18:52 +0900

    [chore] Add u-boot as git submodule

---
+++ u-boot/build.sh
@@ -0,0 +1,8 @@
+cd u-boot
+make qemu-riscv64_defconfig
+CROSS_COMPILE=riscv64-linux-gnu- make -j$(nproc)
+mv u-boot ../../bin/disk/
+
+make qemu-riscv64_smode_defconfig
+CROSS_COMPILE=riscv64-linux-gnu- make -j$(nproc)
+mv u-boot.bin ../../bin/
