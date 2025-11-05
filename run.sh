6ea64503085154cc4e5b9742d0b337fd3947f7fd
nanameki1213 <nanamekitaishin1213@icloud.com>
2025-11-05 14:28:18 +0900

    [feat] Change attached disk image of virtio-blk

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ $QEMU \
   -device virtio-blk-device,drive=drive2,bus=virtio-mmio-bus.1 \
   -drive file=./bin/virt.dtb,if=none,format=raw,id=drive2 \
   -device virtio-blk-device,drive=drive3,bus=virtio-mmio-bus.2 \
-  -drive file=./disk.img,if=none,format=raw,id=drive3 \
+  -drive file=fat:rw:bin/L1disk/,if=none,format=raw,id=drive3 \
   -global virtio-mmio.force-legacy=false \
   -D logfile.log -d in_asm,int \
   # -s -S \
