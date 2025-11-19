bcd6678158d42cb2b30958ff44c384632ee58abf
nanameki1213 <nanamekitaishin1213@icloud.com>
2025-11-19 14:49:22 +0900

    [feat] Remove VS-mode exception handler

--- hypervisor/src/vector.rs
+++ hypervisor/src/vector.rs
@@ -34,12 +34,6 @@ machine_vector_table:
 supervisor_vector_table:
     j supervisor_exception_handler 
     
-.section .text
-.global virtual_supervisor_vector_table
-.balign 256
-virtual_supervisor_vector_table:
-    j virtual_supervisor_exception_handler 
-
 .text
 .global machine_exception_handler 
 .balign 256
@@ -199,82 +193,6 @@ supervisor_exception_handler:
     ld x31, 31*8(sp)
     addi sp, sp, 8*31
     sret
-
-.text
-.global virtual_supervisor_exception_handler 
-.balign 256
-virtual_supervisor_exception_handler:
-    addi sp, sp, -8*31
-    sd x0, 0*8(sp)
-    sd x1, 1*8(sp)
-    sd x2, 2*8(sp)
-    sd x3, 3*8(sp)
-    sd x4, 4*8(sp)
-    sd x5, 5*8(sp)
-    sd x6, 6*8(sp)
-    sd x7, 7*8(sp)
-    sd x8, 8*8(sp)
-    sd x9, 9*8(sp)
-    sd x10, 10*8(sp)
-    sd x11, 11*8(sp)
-    sd x12, 12*8(sp)
-    sd x13, 13*8(sp)
-    sd x14, 14*8(sp)
-    sd x15, 15*8(sp)
-    sd x16, 16*8(sp)
-    sd x17, 17*8(sp)
-    sd x18, 18*8(sp)
-    sd x19, 19*8(sp)
-    sd x20, 20*8(sp)
-    sd x21, 21*8(sp)
-    sd x22, 22*8(sp)
-    sd x23, 23*8(sp)
-    sd x24, 24*8(sp)
-    sd x25, 25*8(sp)
-    sd x26, 26*8(sp)
-    sd x27, 27*8(sp)
-    sd x28, 28*8(sp)
-    sd x29, 29*8(sp)
-    sd x30, 30*8(sp)
-    sd x31, 31*8(sp)
-    csrw sscratch, sp
-    la sp, _intr_stack_end
-    call exception_handler
-    csrr sp, sscratch
-    ld x0, 0*8(sp)
-    ld x1, 1*8(sp)
-    ld x2, 2*8(sp)
-    ld x3, 3*8(sp)
-    ld x4, 4*8(sp)
-    ld x5, 5*8(sp)
-    ld x6, 6*8(sp)
-    ld x7, 7*8(sp)
-    ld x8, 8*8(sp)
-    ld x9, 9*8(sp)
-    ld x10, 10*8(sp)
-    ld x11, 11*8(sp)
-    ld x12, 12*8(sp)
-    ld x13, 13*8(sp)
-    ld x14, 14*8(sp)
-    ld x15, 15*8(sp)
-    ld x16, 16*8(sp)
-    ld x17, 17*8(sp)
-    ld x18, 18*8(sp)
-    ld x19, 19*8(sp)
-    ld x20, 20*8(sp)
-    ld x21, 21*8(sp)
-    ld x22, 22*8(sp)
-    ld x23, 23*8(sp)
-    ld x24, 24*8(sp)
-    ld x25, 25*8(sp)
-    ld x26, 26*8(sp)
-    ld x27, 27*8(sp)
-    ld x28, 28*8(sp)
-    ld x29, 29*8(sp)
-    ld x30, 30*8(sp)
-    ld x31, 31*8(sp)
-    addi sp, sp, 8*31
-    sret
 "
 );
 
@@ -282,11 +200,9 @@ pub fn setup_vector() {
     unsafe extern "C" {
         static machine_vector_table: *const u8;
         static supervisor_vector_table: *const u8;
-        static virtual_supervisor_vector_table: *const u8;
     }
     unsafe { set_mtvec((&machine_vector_table as *const _ as usize) as u64) }
     unsafe { set_stvec((&supervisor_vector_table as *const _ as usize) as u64) }
-    unsafe { set_vstvec((&virtual_supervisor_vector_table as *const _ as usize) as u64) }
 }
 
 fn is_data_abort(scause: usize) -> bool {
