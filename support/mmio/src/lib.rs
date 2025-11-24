62458960672384c6ddf67e318ded5c0fc5140150
nanameki1213 <nanamekitaishin1213@icloud.com>
2025-11-25 07:31:22 +0900

    [feat] Add guest directory

--- support/mmio/src/lib.rs
+++ support/mmio/src/lib.rs
@@ -4,18 +4,9 @@ extern crate alloc;
 
 use alloc::boxed::Box;
 
-pub trait MmioAccess: Sized + Copy + From<u8> + Into<u64> {
-    const SIZE: usize = core::mem::size_of::<Self>();
-}
-
-impl MmioAccess for u8 {}
-impl MmioAccess for u16 {}
-impl MmioAccess for u32 {}
-impl MmioAccess for u64 {}
-
 pub trait MmioHandler {
-    fn read<T: MmioAccess>(&self, offset: usize) -> T;
-    fn write<T: MmioAccess>(&self, offset: usize, value: T);
+    fn read(&self, offset: usize, width: usize) -> usize;
+    fn write(&self, offset: usize, width: usize, value: usize);
 }
 
 struct MmioEntry {
