2ec8aa9d4244e1f60fb4e72c66f8653406f11fcf
nanameki <nanamekitaishin1213@icloud.com>
2026-01-08 12:16:55 +0900

    [feat] separete device tree script l1 vm/l2 vm

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -82,7 +82,7 @@ fn build() -> Result<(), DynError> {
 
         // compile device tree script
         log::info!("compile device tree script for L1 Hypervisor");
-        let dts_path = script_path.clone().join("virt.dts");
+        let dts_path = script_path.clone().join("virt_L1hypervisor.dts");
         let output_path = l1_hypervisor_output_directory.clone().join("virt.dtb");
         device_tree::compile_dts(&dts_path, &output_path)?;
 
