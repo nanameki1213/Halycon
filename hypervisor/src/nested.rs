1705d479c07aff496bd29dc49ee669194098c68c
nanameki <nanamekitaishin1213@icloud.com>
2026-01-18 19:22:44 +0900

    [feat] flush threshold change: 17 to 16

--- hypervisor/src/nested.rs
+++ hypervisor/src/nested.rs
@@ -206,7 +206,7 @@ pub fn reflect_to_l1(
 
         let mut cnt = BUFFER_COUNT.lock();
         *cnt += 1;
-        let do_flush = *cnt > 16;
+        let do_flush = *cnt >= 16;
         if do_flush {
             *cnt = 0;
         }
